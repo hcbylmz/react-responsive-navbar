7cdfef56d3a08686ada115c67772b4da2b9cb8d7
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-09-06 21:05:41 +0300

    react-responsive-navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import magnifier from "../img/loupe.png";
+import sun from "../img/sun_120349.png";
+import "./Navbar.css";
+
+export function Navbar() {
+  const [visible, setVisible] = useState(true);
+  return (
+    <div>
+      <button class="hamburger" onClick={() => setVisible(!visible)}>
+        ≡{" "}
+      </button>
+      <nav className={visible ? " " : "invisible"}>
+        <ul>
+          <li>
+            <button class="home-button">Home</button>
+          </li>
+          <li>
+            <a href="#">Five Columns</a>
+          </li>
+          <li>
+            <a href="#">Listing Page</a>
+          </li>
+          <li>
+            <a href="#">Templates</a>
+          </li>
+          <li>
+            <a href="#">Other Pages</a>
+          </li>
+          <li>
+            <img src={magnifier} />
+          </li>
+          <li>
+            <img src={sun} />
+          </li>
+          <li class="purple">
+            <button class="signup-button">Sign Up</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
